b94abde7fa9681ff3cbb68d563f546020339a5a2
btg3hvyg5b9rtzbzen4j <pavlov.dalibor@proton.me>
2025-12-15 21:12:44 +0100

    initial commit

---
+++ build.ps1
@@ -0,0 +1,11 @@
+Write-Host "Compiling C files..." -ForegroundColor Cyan
+
+$output = gcc (Get-ChildItem *.c | ForEach-Object { $_.FullName }) -o main.exe 2>&1
+
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "Compilation successful!" -ForegroundColor Green
+} else {
+    Write-Host "Compilation failed!`n" -ForegroundColor Red
+    Write-Host "Errors:`n" -ForegroundColor Yellow
+    Write-Host $output -ForegroundColor Red
+}
